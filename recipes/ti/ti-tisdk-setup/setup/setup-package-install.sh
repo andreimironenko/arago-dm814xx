40aa89273e9ada790daa8fd66dd81d9d994ccd1f
Arnie Reynoso <arnier@ti.com>
2010-07-01 15:20:44 -0500

    setup-package-install.sh: Added libcurses5-dev package to setup script
    Signed-off-by: Arnie Reynoso <arnier@ti.com>

--- recipes/ti/ti-tisdk-setup/setup/setup-package-install.sh
+++ recipes/ti/ti-tisdk-setup/setup/setup-package-install.sh
@@ -2,7 +2,7 @@
 
 . `dirname $0`/common.sh
 
-cmd="sudo apt-get install xinetd tftpd nfs-kernel-server minicom build-essential"
+cmd="sudo apt-get install xinetd tftpd nfs-kernel-server minicom build-essential libncurses5-dev"
 echo "--------------------------------------------------------------------------------"
 echo "This step will make sure you have the proper host support packages installed"
 echo "using the following command: $cmd"
