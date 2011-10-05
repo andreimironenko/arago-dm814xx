7a1fdc5c50c2b54bf82d2b47d1958fe93fa572d7
Chase Maupin <Chase.Maupin@ti.com>
2011-10-05 15:59:49 -0500

    arago-gplv3-notice: add script for gplv3 notice
    * Add a startup script that will search the packages installed on
      the target file system and print those packages that are
      licensed under the GPLv3 license.
        * If the license has RLE in it then don't print the package
          since it has the RLE exception.
    * This is to allow adding packages such as oprofile and gdbserver
      to the target file system by default.  This way the user is
      notified both in the manifest as well as when they boot the
      file system that there is GPLv3 content.
    
    Signed-off-by: Chase Maupin <Chase.Maupin@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

---
+++ recipes/arago/arago-gplv3-notice/print-gplv3-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+gplv3_packages=""
+
+# find all files with GPLv3 in them that do not also have RLE
+# exception listed.
+files=`grep -i GPLv3 /usr/lib/opkg/info/*.control | grep -v RLE | cut -d: -f1`
+
+for i in $files
+do
+    package=`basename $i | sed s/\.control//`
+    gplv3_packages="$gplv3_packages""\t$package\n"
+done
+
+if [ "$gplv3_packages" != "" ]
+then
+    echo "***************************************************************"
+    echo "***************************************************************"
+    echo "NOTICE: This file system contains the followin GPLv3 packages:"
+    echo -e $gplv3_packages
+    echo "If you do not wish to distribute GPLv3 components please remove"
+    echo "the above packages prior to distribution.  This can be done using"
+    echo "the opkg remove command.  i.e.:"
+    echo "    opkg remove <package>"
+    echo "Where <package> is the name printed in the list above"
+    echo "***************************************************************"
+    echo "***************************************************************"
+fi
