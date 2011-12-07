863a7a09bb4b4b37b0b65b520040ae5a63c3deb3
Siddharth Heroor <heroor@ti.com>
2011-12-07 18:39:04 +0530

    ti-tisdk-mksdboot: Add colorbar command at the start

--- recipes/ti/ti-tisdk-mksdboot/ti814x/mksdboot.sh
+++ recipes/ti/ti-tisdk-mksdboot/ti814x/mksdboot.sh
@@ -139,6 +139,7 @@ fi
 # creating boot.scr
 execute "mkdir -p /tmp/sdk"
 cat <<EOF >/tmp/sdk/boot.cmd
+colorbar
 setenv bootargs 'console=ttyO0,115200n8 rootwait root=/dev/mmcblk0p2 rw mem=256M earlyprintk notifyk.vpssm3_sva=0xBF900000 vram=50M ti814xfb.vram=0:16M,1:16M,2:6M ip=off noinitrd'
 fatload mmc 0 0x80009000 uImage
 bootm 0x80009000
