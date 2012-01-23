51bdd03f1ed5079b3a7a97af3704923a893e26ea
Siddharth Heroor <heroor@ti.com>
2012-01-23 13:37:07 +0530

    ti-tisdk-setup: Add support for Oneiric and Lucid
    * Allow customers to install on both Lucid and Oneiric. General
      strategy is to support Lucid till we get Precise. Once Precise
      is available we support Precise and the latest Ubuntu release.

--- recipes/ti/ti-tisdk-setup/setup-host-check.sh
+++ recipes/ti/ti-tisdk-setup/setup-host-check.sh
@@ -4,10 +4,15 @@ echo
 echo "--------------------------------------------------------------------------------"
 echo "Verifying Linux host distribution"
 host=`lsb_release -a 2>/dev/null | grep Codename: | awk {'print $2'}`
-if [ "$host" != "lucid" ]; then
-    echo "Unsupported host machine, only Ubuntu 10.04 LTS supported"
+if [ "$host" == "lucid" ]; then
+    echo "Ubuntu 10.04 LTS found successfully, continuing.."
+    echo "--------------------------------------------------------------------------------"
+    echo
+elif [ "$host" == "oneiric" ]; then
+    echo "Ubuntu 11.10 found successfully, continuing.."
+    echo "--------------------------------------------------------------------------------"
+    echo
+else
+    echo "Unsupported host machine, only Ubuntu 10.04 LTS and Ubuntu 11.10 supported"
     exit 1
 fi
-echo "Ubuntu 10.04 LTS found successfully, continuing.."
-echo "--------------------------------------------------------------------------------"
-echo
