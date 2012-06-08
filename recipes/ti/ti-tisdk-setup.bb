14f35ccd7ae3ff8d0ae4db1bf4f994706a6e3313
Siddharth Heroor <heroor@ti.com>
2012-06-08 16:57:31 +0530

    ti-tisdk-setup: Add support for C6A811x
    * Based of TI814x

--- recipes/ti/ti-tisdk-setup.bb
+++ recipes/ti/ti-tisdk-setup.bb
@@ -2,7 +2,7 @@ DESCRIPTION = "Package contain Makefile and Rule.make used for building DVSDK co
 LICENSE = "TI"
 
 require ti-paths.inc
-COMPATIBLE_MACHINE = "(omap3evm|am37x-evm|dm37x-evm|dm365-evm|omapl138|am3517-evm|dm368-evm|beagleboard|ti33x|ti816x|ti814x)"
+COMPATIBLE_MACHINE = "(omap3evm|am37x-evm|dm37x-evm|dm365-evm|omapl138|am3517-evm|dm368-evm|beagleboard|ti33x|ti816x|ti814x|ti811x)"
 
 UBOOT_ENV_dm365-evm = "setup-uboot-env-dm365.sh"
 UBOOT_ENV_dm368-evm = "setup-uboot-env-dm365.sh"
@@ -11,6 +11,7 @@ UBOOT_ENV_dm37x-evm = "setup-uboot-env-dm3730.sh"
 UBOOT_ENV_omap3evm = "setup-uboot-env-omap3530.sh"
 UBOOT_ENV_ti816x = "setup-uboot-env-ti816x.sh"
 UBOOT_ENV_ti814x = "setup-uboot-env-ti814x.sh"
+UBOOT_ENV_ti811x = "setup-uboot-env-ti811x.sh"
 UBOOT_ENV_am37x-evm = "setup-uboot-env-am37x.sh"
 UBOOT_ENV_beagleboard = "setup-uboot-env-beagleboard.sh"
 UBOOT_ENV_am3517-evm = "setup-uboot-env-am3517.sh"
@@ -21,6 +22,7 @@ UBOOT_ENV_ti33x = "setup-uboot-env-am335x.sh"
 
 SETUP_NAND_ti816x = "setup-nand-images-dm816x.sh"
 SETUP_NAND_ti814x = "setup-nand-images-dm814x.sh"
+SETUP_NAND_ti811x = "setup-nand-images-c6a811x.sh"
 
 SRC_URI = "\
 	file://setup.sh \
---
+++ recipes/ti/ti-tisdk-setup/setup-nand-images-c6a811x.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+
+cwd=`dirname $0`
+. $cwd/common.sh
+prompt="TI8148_EVM#"
+
+#Memory address for loading the images
+loadaddr=0x81000000
+
+#offset to partition for u-boot.min.nand in NAND 
+uboot_min_offset=0x0
+
+#offset to partition for kernel in NAND 
+kernel_offset=0x00280000
+
+#size of the partition for uboot.min.sd
+partition_uboot_min=0x00020000
+
+#size of the partition for uboot.bin
+partition_uboot_bin=0x00240000
+
+#size of the partition for kernel
+partition_kernel=0x00440000
+
+#size of the u-boot.min.nand with a buffer of 4KB along the size of default image
+uboot_min_size=0x00018000
+
+#size of the u-boot.bin with a buffer of 4KB along the size of default image
+uboot_bin_size=0x00034800
+
+#size of the uImage with a buffer of 4KB along the size of default image
+kernel_size=0x002AC000
+
+do_expect() 
+{
+    echo "expect {" >> $3
+    check_status
+    echo "    $1" >> $3
+    check_status
+    echo "}" >> $3
+    check_status
+    echo $2 >> $3
+    check_status
+    echo >> $3
+}
+
+echo
+echo "--------------------------------------------------------------------------------"
+echo "This step will create a minicom script for flashing images to NAND"
+echo
+
+echo "Would you like to flash the kernel image to NAND (y/n)?"
+
+read -p "[ y ] " kernel
+echo
+
+if [ ! -n "$kernel" ]; then
+    kernel="y"
+fi
+
+if [ "$kernel" == "y" ]; then
+    cfg="u-boot_uImage-nand"
+else 
+    cfg="u-boot-nand"
+fi
+
+flashfile=setup_$cfg.minicom
+flashfilepath=$cwd/../$flashfile
+   
+if [ -f $flashfilepath ]; then
+    echo "Deleting the old $flashfile"
+    rm -f $flashfilepath 
+    check_status
+fi
+
+timeout=300
+echo "timeout $timeout" >> $flashfilepath
+echo "verbose on" >> $flashfilepath
+echo >> $flashfilepath
+do_expect "\"$prompt\"" "send \"mmc rescan 0\"" $flashfilepath   
+do_expect "\"$prompt\"" "send \"mw.b $loadaddr 0xFF $partition_uboot_min\"" $flashfilepath
+do_expect "\"$prompt\"" "send \"fatload mmc 0 $loadaddr u-boot.min.nand\"" $flashfilepath
+do_expect "\"$prompt\"" "send \"nand erase $uboot_min_offset $partition_uboot_min\"" $flashfilepath
+do_expect "\"$prompt\"" "send \"nand write.i $loadaddr $uboot_min_offset $uboot_min_size\"" $flashfilepath
+do_expect "\"$prompt\"" "send \"mw.b $loadaddr 0xFF $partition_uboot_bin\"" $flashfilepath
+do_expect "\"$prompt\"" "send \"fatload mmc 0 $loadaddr u-boot.bin\"" $flashfilepath
+do_expect "\"$prompt\"" "send \"nand erase $partition_uboot_min $partition_uboot_bin\"" $flashfilepath
+do_expect "\"$prompt\"" "send \"nand write.i $loadaddr $partition_uboot_min $uboot_bin_size\"" $flashfilepath
+
+if [ "$kernel" = "y" ]; then 
+    do_expect "\"$prompt\"" "send \"mw.b $loadaddr 0xFF $partition_kernel\"" $flashfilepath
+    do_expect "\"$prompt\"" "send \"fatload mmc 0 $loadaddr uImage\"" $flashfilepath
+    do_expect "\"$prompt\"" "send \"nand erase $kernel_offset $partition_kernel\"" $flashfilepath
+    do_expect "\"$prompt\"" "send \"nand write.i $loadaddr $kernel_offset $kernel_size\"" $flashfilepath
+fi
+
+do_expect "\"$prompt\"" "! killall -s SIGHUP minicom" $flashfilepath
+
+echo "Successfully wrote $flashfile"
+echo
+echo "Would you like to run the setup script now (y/n)? This requires you to connect"
+echo "the RS-232 cable between your host and EVM as well as your ethernet cable as"
+echo "described in the Quick Start Guide. Please ensure that SW2 switch for NAND is"
+echo "in ON position and EVM switch settings is for SD boot mode."
+echo
+echo "Once answering 'y' on the prompt below you will have $timeout seconds to connect"
+echo "the board and power cycle it before the setup times out"
+echo
+echo "Press any key to stop autoboot of the uboot second stage. Once the autoboot is"
+echo "stopped, this script will automatically set parameters."
+echo
+echo "After successfully executing this script, your EVM will be set up. You will be "
+echo "able to connect to it by executing 'minicom -w' or if you prefer a windows host"
+echo "you can set up Tera Term as explained in the Software Developer's Guide."
+echo "If you connect minicom or Tera Term and power cycle the board Linux will boot."
+echo
+read -p "[ y ] " minicomsetup
+
+if [ ! -n "$minicomsetup" ]; then
+    minicomsetup="y"
+fi
+
+if [ "$minicomsetup" == "y" ]; then
+    pushd $cwd/..
+    check_status
+    minicom -S $flashfile
+    popd >> /dev/null
+    check_status
+fi
+
+echo "You can manually run minicom in the future with this setup script using: minicom -S $flashfile"
